1a6262bd02fb1267758f14433d9561357c4541e9
BeltedKingfisher <AmyJSeibert@gmail.com>
2021-07-22 14:07:08 -0500

    Finished Studio

--- index.js
+++ index.js
@@ -12,14 +12,36 @@ function getData() {
 function AstronautBios(astronaut) {
 
   return (
-    <div>
-
+    <div className="astronaut">
+      <div className="bio">
+       <h3>{astronaut.firstName} {astronaut.lastName}</h3>
+       <ul>
+          <li>Hours In Space: {astronaut.hoursInSpace}</li>
+          <li>Active: {astronaut.active.toString()}</li>
+          <li>Skills: {astronaut.skills.toString().split(',').join(', ')}</li>
+       </ul>
+      </div>
+      <img className="avatar" src={astronaut.picture}/>
     </div>
-  )
+  );
 }
 
 // Function should render all astronaut bios
 function renderBios(data) {
+  const root = document.getElementById('root');
+  const container = 
+  <div className='container'>
+    {AstronautBios(data[0])}
+    {AstronautBios(data[1])}
+    {AstronautBios(data[2])}
+    {AstronautBios(data[3])}
+    {AstronautBios(data[4])}
+    {AstronautBios(data[5])}
+    {AstronautBios(data[6])}
+  </div>
+  
+  ReactDOM.render( container, root ); 
+  
   //The AstronautBios component should be repeated to display bios for all 
   //astronaunts in the returned data object.
   
